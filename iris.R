7634f6b303fff710c03d29ffd9e8913245e3ddf5
Sean McKinley <seansmckinley@gmail.com>
2015-08-03 21:48:43 -0700

    unsupervised learning to supervised

--- iris.R
+++ iris.R
@@ -14,7 +14,7 @@ invisible(if (file.exists("importance.csv")){
 })
 
 for (i in 1:25){
-    test  <- iris[ c(1:10, 51:60, 101:110), -5]  # removed the Species column here.
+    test  <- iris[ c(1:10, 51:60, 101:110), ]  # removed the Species column here.
     permute <- c(sample(11:50), sample(61:100), sample(111:150))
     train <- iris[ permute,]
     iris.rf <- randomForest(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, 
