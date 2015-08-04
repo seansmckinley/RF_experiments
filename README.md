4f5bf5f8dae083fbfeb1c4b59535928b35af9248
Sean McKinley <seansmckinley@gmail.com>
2015-08-04 10:06:39 -0700

    same seed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RF_experiments
+
+With the same seed, the result of the importance function call on the same randomForest object yields identical output
+even when the order of the training set is shuffled.
--- iris.R
+++ iris.R
@@ -15,7 +15,7 @@ invisible(if (file.exists("importance.csv")){
 
 for (i in 1:25){
     set.seed(250)
-    test  <- iris[ c(1:10, 51:60, 101:110), ]  # removed the Species column here.
+    test  <- iris[ c(1:10, 51:60, 101:110), ]  
     permute <- c(sample(11:50), sample(61:100), sample(111:150))
     train <- iris[ permute,]
     iris.rf <- randomForest(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, 
@@ -24,8 +24,8 @@ for (i in 1:25){
     do.trace=1000, 
     ntree = 1000) 
     imp <- importance(iris.rf)
-    write.table(imp, file = "importance.csv", append = T, sep =" ", eol ="\n", row.names = T, col.names = T)
-    write.table(c("\n"), file = "importance.csv", append = T, sep =" ", eol ="\n", row.names = F, col.names = F)
+    write.table(imp, file = "importance.csv", append = T, sep =", ", eol ="\n", row.names = T, col.names = T)
+    write.table(c("\n"), file = "importance.csv", append = T, sep =", ", eol ="\n", row.names = F, col.names = F)
 
 }
 
